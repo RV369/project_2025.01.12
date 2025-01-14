678a25b4aa48b434cbcd97c8fccc4c111aab2560
Yury Demidov <laikr@rambler.ru>
2025-01-14 20:50:40 +0300

    2025/01/14

--- src/main.py
+++ src/main.py
@@ -1,14 +1,35 @@
 from fastapi import Body, FastAPI
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-
-from .core import data_pipline
+from transformers import pipeline
 
 app = FastAPI()
 
 
 class Schema(BaseModel):
     text: str | list[str]
+
+
+clf = pipeline(
+    task='sentiment-analysis',
+    model='SkolkovoInstitute/russian_toxicity_classifier',
+)
+
+
+def data_pipline(text):
+    data_rezult = []
+    if type(text) is str:
+        for sentence in text.split('.'):
+            if sentence != '':
+                rezult = clf(sentence)
+                rezult[0].update({'string': f'{sentence}'})
+                data_rezult.append(rezult)
+            else:
+                continue
+    else:
+        for i in text:
+            data_rezult.append(data_pipline(i))
+    return data_rezult
 
 
 @app.post('/', response_model=Schema)
